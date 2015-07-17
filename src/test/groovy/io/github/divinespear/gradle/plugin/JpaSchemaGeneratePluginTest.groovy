4f80d3984b2454fbb11254719b87ab20b848bdcb
Sin-young "Divinespear" Kang <divinespear@gmail.com>
2015-07-17 22:50:06 +0900

    merge some tests

--- src/test/groovy/io/github/divinespear/gradle/plugin/JpaSchemaGeneratePluginTest.groovy
+++ src/test/groovy/io/github/divinespear/gradle/plugin/JpaSchemaGeneratePluginTest.groovy
@@ -24,15 +24,16 @@ import static org.junit.Assert.*
 import io.github.divinespear.gradle.plugin.config.Configuration
 import nebula.test.ProjectSpec
 
-import org.gradle.testfixtures.ProjectBuilder
 import org.junit.Test
 
 class JpaSchemaGeneratePluginTest extends ProjectSpec {
+    
+    def setup() {
+        project.apply plugin: "jpa-schema-generate"
+    }
 
     @Test
     void shouldPluginAddsTaskToProject() {
-        project.apply plugin: "jpa-schema-generate"
-
         // task
         assertThat(project.tasks.generateSchema, instanceOf(JpaSchemaGenerateTask))
         // extensions
@@ -44,8 +45,6 @@ class JpaSchemaGeneratePluginTest extends ProjectSpec {
 
     @Test
     void shouldConfigGenerateSchemaTargets() {
-        project.apply plugin: "jpa-schema-generate"
-
         project.generateSchema {
             targets {
                 script {
@@ -62,5 +61,83 @@ class JpaSchemaGeneratePluginTest extends ProjectSpec {
         assertThat(project.generateSchema.targets.size(), is(2))
         def schemaTargets = project.tasks.generateSchema.targets
         assertThat(schemaTargets.size(), is(2))
+    }
+
+
+    static final String linesep = System.properties["line.separator"]?:"\n"
+
+    /* issue 8 */
+
+    @Test
+    void "issue #8 - shouldFormatCreateTable"() {
+        def from = "CREATE TABLE SYSTEM_CURRENCY_RATE_HISTORY (CREATED_DATE DATETIME NULL,LAST_MODIFIED_DATE DATETIME NULL,RATE NUMERIC(28) NULL,VERSION NUMERIC(19) NOT NULL,REFERENCE_ID VARCHAR(255) NOT NULL,CREATED_BY VARCHAR(36) NULL,LAST_MODIFIED_BY VARCHAR(36) NULL,PRIMARY KEY (VERSION,REFERENCE_ID));"
+        def expected = """CREATE TABLE SYSTEM_CURRENCY_RATE_HISTORY (
+\tCREATED_DATE DATETIME NULL,
+\tLAST_MODIFIED_DATE DATETIME NULL,
+\tRATE NUMERIC(28) NULL,
+\tVERSION NUMERIC(19) NOT NULL,
+\tREFERENCE_ID VARCHAR(255) NOT NULL,
+\tCREATED_BY VARCHAR(36) NULL,
+\tLAST_MODIFIED_BY VARCHAR(36) NULL,
+\tPRIMARY KEY (VERSION,REFERENCE_ID)
+);"""
+        def task = project.tasks.generateSchema
+        assertThat(task.format(from, linesep), is(expected))
+    }
+
+    @Test
+    void "issue #8 - shouldFormatCreateTableEx"() {
+        def from = "CREATE TEMPORARY TABLE SYSTEM_CURRENCY_RATE_HISTORY (CREATED_DATE DATETIME NULL,LAST_MODIFIED_DATE DATETIME NULL,RATE NUMERIC(28) NULL,VERSION NUMERIC(19) NOT NULL,REFERENCE_ID VARCHAR(255) NOT NULL,CREATED_BY VARCHAR(36) NULL,LAST_MODIFIED_BY VARCHAR(36) NULL,PRIMARY KEY (VERSION,REFERENCE_ID));"
+        def expected = """CREATE TEMPORARY TABLE SYSTEM_CURRENCY_RATE_HISTORY (
+\tCREATED_DATE DATETIME NULL,
+\tLAST_MODIFIED_DATE DATETIME NULL,
+\tRATE NUMERIC(28) NULL,
+\tVERSION NUMERIC(19) NOT NULL,
+\tREFERENCE_ID VARCHAR(255) NOT NULL,
+\tCREATED_BY VARCHAR(36) NULL,
+\tLAST_MODIFIED_BY VARCHAR(36) NULL,
+\tPRIMARY KEY (VERSION,REFERENCE_ID)
+);"""
+        def task = project.tasks.generateSchema
+        assertThat(task.format(from, linesep), is(expected))
+    }
+
+    @Test
+    void "issue #8 - shouldFormatCreateIndex"() {
+        def from = "CREATE INDEX INDEX_USER_ACCOUNT_ENABLED_DELETED ON USER_ACCOUNT (ENABLED,DELETED);"
+        def expected = """CREATE INDEX INDEX_USER_ACCOUNT_ENABLED_DELETED
+\tON USER_ACCOUNT (ENABLED,DELETED);"""
+        def task = project.tasks.generateSchema
+        assertThat(task.format(from, linesep), is(expected))
+    }
+
+    @Test
+    void "issue #8 - shouldFormatCreateIndexEx"() {
+        def from = "CREATE UNIQUE INDEX INDEX_USER_ACCOUNT_ENABLED_DELETED ON USER_ACCOUNT (ENABLED,DELETED);"
+        def expected = """CREATE UNIQUE INDEX INDEX_USER_ACCOUNT_ENABLED_DELETED
+\tON USER_ACCOUNT (ENABLED,DELETED);"""
+        def task = project.tasks.generateSchema
+        assertThat(task.format(from, linesep), is(expected))
+    }
+
+    @Test
+    void "issue #8 - shouldFormatAlterTable"() {
+        def from = "ALTER TABLE PRODUCT_CATEGORY ADD CONSTRAINT PRODUCTCATEGORYPRENTID FOREIGN KEY (PARENT_ID) REFERENCES PRODUCT_CATEGORY (ID);"
+        def expected = """ALTER TABLE PRODUCT_CATEGORY
+\tADD CONSTRAINT PRODUCTCATEGORYPRENTID FOREIGN KEY (PARENT_ID)
+\tREFERENCES PRODUCT_CATEGORY (ID);"""
+        def task = project.tasks.generateSchema
+        assertThat(task.format(from, linesep), is(expected))
+    }
+
+    /* issue 9 */
+
+    @Test
+    void "issue #9 - shouldOverrideCreateIndex"() {
+        def from = "CREATE INDEX INDEX_SYSTEM_CURRENCY_RATE_VERSION DESC ON SYSTEM_CURRENCY_RATE (VERSION DESC);"
+        def expected = """CREATE INDEX INDEX_SYSTEM_CURRENCY_RATE_VERSION
+\tON SYSTEM_CURRENCY_RATE (VERSION DESC);"""
+        def task = project.tasks.generateSchema
+        assertThat(task.format(from, linesep), is(expected))
     }
 }
