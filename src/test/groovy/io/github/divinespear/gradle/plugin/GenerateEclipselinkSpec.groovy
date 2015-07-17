a0624897b7244907326626c373c5e6bd97da520d
Sin-young "Divinespear" Kang <divinespear@gmail.com>
2015-07-18 01:30:40 +0900

    merge tests, separate test (expected) result data

--- src/test/groovy/io/github/divinespear/gradle/plugin/GenerateEclipselinkSpec.groovy
+++ src/test/groovy/io/github/divinespear/gradle/plugin/GenerateEclipselinkSpec.groovy
@@ -48,7 +48,7 @@ sourceSets {
 }
 
 generateSchema {
-    databaseAction = "drop-and-create"
+    scriptAction = "drop-and-create"
     targets {
         h2script {
             databaseProductName = "H2"
@@ -58,6 +58,8 @@ generateSchema {
             dropOutputFileName = "h2-drop.sql"
         }
         h2database {
+            databaseAction = "drop-and-create"
+            scriptAction = null
             jdbcDriver = "org.h2.Driver"
             jdbcUrl = "jdbc:h2:\${buildDir}/generated-schema/test;AUTO_SERVER=TRUE"
             jdbcUser = "sa"
@@ -141,4 +143,88 @@ generateSchema {
         }
         expect == actual.toString()
     }
+
+    def shouldWorkFormatted() {
+        given:
+        buildFile << """
+
+sourceSets {
+    main {
+        java {
+            srcDir file("../../../../src/test/resources/unit/eclipselink/src")
+        }
+        resources {
+            srcDir file("../../../../src/test/resources/unit/eclipselink/resources")
+        }
+        output.resourcesDir output.classesDir
+    }
+}
+
+generateSchema {
+    format = true
+    scriptAction = "drop-and-create"
+    targets {
+        h2script {
+            databaseProductName = "H2"
+            databaseMajorVersion = 1
+            databaseMinorVersion = 4
+            createOutputFileName = "h2-create.sql"
+            dropOutputFileName = "h2-drop.sql"
+        }
+        mysql5 {
+            databaseProductName = "MySQL"
+            databaseMajorVersion = 5
+            databaseMinorVersion = 1
+        }
+        postgres {
+            databaseProductName = "PostgreSQL"
+            databaseMajorVersion = 9
+            databaseMinorVersion = 0
+        }
+        "oracle-11g" {
+            databaseProductName = "Oracle"
+            databaseMajorVersion = 11
+            databaseMinorVersion = 0
+        }
+        "oracle-10g" {
+            databaseProductName = "Oracle"
+            databaseMajorVersion = 10
+            databaseMinorVersion = 0
+        }
+        "oracle-9i" {
+            databaseProductName = "Oracle"
+            databaseMajorVersion = 9
+            databaseMinorVersion = 0
+        }
+        "oracle-8i" {
+            databaseProductName = "Oracle"
+            databaseMajorVersion = 8
+            databaseMinorVersion = 0
+        }
+        mssql {
+            databaseProductName = "Microsoft SQL Server"
+            databaseMajorVersion = 10
+            databaseMinorVersion = 0
+        }
+    }
+}
+"""
+
+        when:
+        runTasksSuccessfully "generateSchema"
+
+        then:
+        // ddl script
+        def expect = file("../../../../src/test/resources/eclipselink-format-result.txt").text
+        def actual = StringBuilder.newInstance()
+        ["h2", "mysql5", "postgres", "oracle-11g", "oracle-10g", "oracle-9i", "oracle-8i", "mssql"].each {
+            [it + "-create.sql", it + "-drop.sql"].each {
+                file("build/generated-schema/" + it).exists()
+                actual << "-- " + it + "\n"
+                actual << file("build/generated-schema/" + it).text
+            }
+        }
+        expect == actual.toString()
+    }
+
 }
