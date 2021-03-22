698f524426e7bf3f1ebcf80f1818b7e81f7508f5
Sinyoung "Divinespear" Kang <divinespear@gmail.com>
2021-03-22 09:36:01 +0900

    rewriting test cases * rewrite abstract specs

---
+++ src/functionalTest/kotlin/test/spec/GroovyFunctionalSpec.kt
@@ -0,0 +1,4 @@
+package test.spec
+
+abstract class GroovyFunctionalSpec : FunctionalSpec() {
+}
---
+++ src/functionalTest/kotlin/test/spec/KotlinFunctionalSpec.kt
@@ -0,0 +1,4 @@
+package test.spec
+
+abstract class KotlinFunctionalSpec: FunctionalSpec("gradle.kts") {
+}
