5238bc49540e4010325960359b7534c74f23c11d
Sinyoung "Divinespear" Kang <divinespear@gmail.com>
2021-04-15 03:00:44 +0900

    add entity source and persistence.xml files for test

---
+++ src/functionalTest/resources/java/io/github/divinespear/model/ManyColumns.java
@@ -0,0 +1,323 @@
+package io.github.divinespear.model;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * Test JPA Model
+ * 
+ * @author divinespear
+ */
+@Entity
+@Access(AccessType.FIELD)
+@Table(name = "MANY_COLUMN_TABLE")
+public class ManyColumns {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String column00;
+    private String column01;
+    private String column02;
+    private String column03;
+    private String column04;
+    private String column05;
+    private String column06;
+    private String column07;
+    private String column08;
+    private String column09;
+    private String column10;
+    private String column11;
+    private String column12;
+    private String column13;
+    private String column14;
+    private String column15;
+    private String column16;
+    private String column17;
+    private String column18;
+    private String column19;
+    private String column20;
+    private String column21;
+    private String column22;
+    private String column23;
+    private String column24;
+    private String column25;
+    private String column26;
+    private String column27;
+    private String column28;
+    private String column29;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getColumn00() {
+        return column00;
+    }
+
+    public void setColumn00(String column00) {
+        this.column00 = column00;
+    }
+
+    public String getColumn01() {
+        return column01;
+    }
+
+    public void setColumn01(String column01) {
+        this.column01 = column01;
+    }
+
+    public String getColumn02() {
+        return column02;
+    }
+
+    public void setColumn02(String column02) {
+        this.column02 = column02;
+    }
+
+    public String getColumn03() {
+        return column03;
+    }
+
+    public void setColumn03(String column03) {
+        this.column03 = column03;
+    }
+
+    public String getColumn04() {
+        return column04;
+    }
+
+    public void setColumn04(String column04) {
+        this.column04 = column04;
+    }
+
+    public String getColumn05() {
+        return column05;
+    }
+
+    public void setColumn05(String column05) {
+        this.column05 = column05;
+    }
+
+    public String getColumn06() {
+        return column06;
+    }
+
+    public void setColumn06(String column06) {
+        this.column06 = column06;
+    }
+
+    public String getColumn07() {
+        return column07;
+    }
+
+    public void setColumn07(String column07) {
+        this.column07 = column07;
+    }
+
+    public String getColumn08() {
+        return column08;
+    }
+
+    public void setColumn08(String column08) {
+        this.column08 = column08;
+    }
+
+    public String getColumn09() {
+        return column09;
+    }
+
+    public void setColumn09(String column09) {
+        this.column09 = column09;
+    }
+
+    public String getColumn10() {
+        return column10;
+    }
+
+    public void setColumn10(String column10) {
+        this.column10 = column10;
+    }
+
+    public String getColumn11() {
+        return column11;
+    }
+
+    public void setColumn11(String column11) {
+        this.column11 = column11;
+    }
+
+    public String getColumn12() {
+        return column12;
+    }
+
+    public void setColumn12(String column12) {
+        this.column12 = column12;
+    }
+
+    public String getColumn13() {
+        return column13;
+    }
+
+    public void setColumn13(String column13) {
+        this.column13 = column13;
+    }
+
+    public String getColumn14() {
+        return column14;
+    }
+
+    public void setColumn14(String column14) {
+        this.column14 = column14;
+    }
+
+    public String getColumn15() {
+        return column15;
+    }
+
+    public void setColumn15(String column15) {
+        this.column15 = column15;
+    }
+
+    public String getColumn16() {
+        return column16;
+    }
+
+    public void setColumn16(String column16) {
+        this.column16 = column16;
+    }
+
+    public String getColumn17() {
+        return column17;
+    }
+
+    public void setColumn17(String column17) {
+        this.column17 = column17;
+    }
+
+    public String getColumn18() {
+        return column18;
+    }
+
+    public void setColumn18(String column18) {
+        this.column18 = column18;
+    }
+
+    public String getColumn19() {
+        return column19;
+    }
+
+    public void setColumn19(String column19) {
+        this.column19 = column19;
+    }
+
+    public String getColumn20() {
+        return column20;
+    }
+
+    public void setColumn20(String column20) {
+        this.column20 = column20;
+    }
+
+    public String getColumn21() {
+        return column21;
+    }
+
+    public void setColumn21(String column21) {
+        this.column21 = column21;
+    }
+
+    public String getColumn22() {
+        return column22;
+    }
+
+    public void setColumn22(String column22) {
+        this.column22 = column22;
+    }
+
+    public String getColumn23() {
+        return column23;
+    }
+
+    public void setColumn23(String column23) {
+        this.column23 = column23;
+    }
+
+    public String getColumn24() {
+        return column24;
+    }
+
+    public void setColumn24(String column24) {
+        this.column24 = column24;
+    }
+
+    public String getColumn25() {
+        return column25;
+    }
+
+    public void setColumn25(String column25) {
+        this.column25 = column25;
+    }
+
+    public String getColumn26() {
+        return column26;
+    }
+
+    public void setColumn26(String column26) {
+        this.column26 = column26;
+    }
+
+    public String getColumn27() {
+        return column27;
+    }
+
+    public void setColumn27(String column27) {
+        this.column27 = column27;
+    }
+
+    public String getColumn28() {
+        return column28;
+    }
+
+    public void setColumn28(String column28) {
+        this.column28 = column28;
+    }
+
+    public String getColumn29() {
+        return column29;
+    }
+
+    public void setColumn29(String column29) {
+        this.column29 = column29;
+    }
+
+}
