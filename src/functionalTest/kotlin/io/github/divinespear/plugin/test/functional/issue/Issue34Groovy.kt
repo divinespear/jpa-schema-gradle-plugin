f1edcd1fe6d8f41538af060e609e3c45aa931b25
Sinyoung "Divinespear" Kang <divinespear@gmail.com>
2021-04-16 02:51:51 +0900

    migrate issue-34 test case for kotlin and groovy

---
+++ src/functionalTest/kotlin/io/github/divinespear/plugin/test/functional/issue/Issue34Groovy.kt
@@ -0,0 +1,90 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package io.github.divinespear.plugin.test.functional.issue
+
+import io.github.divinespear.plugin.test.GroovyFunctionalSpec
+import io.kotest.core.test.TestType
+import io.kotest.matchers.should
+import io.kotest.matchers.shouldBe
+import org.gradle.testkit.runner.TaskOutcome
+import java.io.File
+
+class Issue34Groovy : GroovyFunctionalSpec() {
+
+  init {
+    beforeTest {
+      if (it.type === TestType.Test) {
+        val mainJavaDir = testProjectDir.resolve("src/main/java").apply {
+          mkdirs()
+        }
+        val resourceJavaDir = File("src/functionalTest/resources/java")
+        resourceJavaDir.copyRecursively(mainJavaDir, true)
+
+        buildFile.appendText(
+          """
+            
+            plugins {
+              id 'io.github.divinespear.jpa-schema-generate'
+            }
+            
+            repositories {
+              mavenCentral()
+            }
+            
+          """.trimIndent()
+        )
+      }
+    }
+
+    "task" should {
+      "work with default target" {
+        buildFile.appendText(
+          """
+            
+            dependencies {
+              compile 'org.springframework.boot:spring-boot-starter-data-jpa:2.0.0.RELEASE'
+              runtime 'com.h2database:h2:1.4.191'
+              runtime fileTree(dir: "../../../lib", include: "*.jar")
+            }
+            
+            generateSchema {
+              vendor = 'hibernate'
+              packageToScan = [ 'io.github.divinespear.model' ]
+              scriptAction = "drop-and-create"
+              properties = [
+                'hibernate.physical_naming_strategy' : 'org.springframework.boot.orm.jpa.hibernate.SpringPhysicalNamingStrategy',
+                'hibernate.id.new_generator_mappings': 'false'
+              ]
+              databaseProductName = "H2"
+              databaseMajorVersion = 1
+              databaseMinorVersion = 4
+              createOutputFileName = "h2-create.sql"
+              dropOutputFileName = "h2-drop.sql"
+            }
+          """.trimIndent()
+        )
+
+        val result = runGenerateSchemaTask()
+        result.task(":generateSchema") should {
+          it?.outcome shouldBe TaskOutcome.SUCCESS
+        }
+      }
+    }
+  }
+}
